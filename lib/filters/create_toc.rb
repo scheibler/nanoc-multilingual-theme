1158cd21b18076ed8eae6abb5f74e0b6c3667fc4
Eric Scheibler <email@eric-scheibler.de>
2024-03-11 02:04:28 +0100

    Version 0.2.3:
    Improvements in the table-of-contents creation function:
    - Doesn't need a trailing # at the end of the headings any more
    - Filters empty links (<a ...></a>) from the heading text (these links can be used as local anchors in the html document)
    
    Bug fixes:
    - Trailing // removed from the main navigation links

--- lib/filters/create_toc.rb
+++ lib/filters/create_toc.rb
@@ -1,27 +1,40 @@
 require 'uri'
 class CreateTOC < Nanoc::Filter
     identifier :create_toc
-    def run(content, params={})
-        toc = "## %s ##\n\n" % translate_string(language_code_of(item), 'heading_table_of_contents')
+
+    def run(content="", params={})
+        toc = "## %s\n\n" % translate_string(language_code_of(item), 'heading_table_of_contents')
         hlevel2 = 0
         hlevel3 = 0
-        content.scan(/^(\#{2,3}) (.*) (\#{2,3})$/).each do |match|
-            if match[0] != match[2]
-                continue
-            end
+
+        content.scan(/^(\#{2,3}) (.*)$/).each do |match|
             if match[0] == "###" and hlevel2 == 0
-                continue
+                # found h3 before first h2, ignore
+                next
             end
+
+            htext = match[1] \
+                # remove html link tag, used for local document anchors
+                .gsub(/<a.*>.*<\/a>/, '') \
+                # remove trailing #
+                .gsub(/#+\z/, '') \
+                .strip
+            hid = htext \
+                .downcase \
+                .gsub(' ', '-') \
+                .gsub(/[^\p{L}\d\.-]/, '') \
+                .strip
+
             if match[0] == "##"
                 hlevel2 += 1
                 hlevel3 = 0
-                toc << "* [%d. %s](#%s)\n" % [hlevel2, match[1], CGI.escape(match[1].downcase.gsub(' ', '-'))]
-            end
-            if match[0] == "###"
+                toc << "* [%d. %s](#%s)\n" % [hlevel2, htext, hid]
+            else
                 hlevel3 += 1
-                toc << "    + [%d.%d %s](#%s)\n" % [hlevel2, hlevel3, match[1], CGI.escape(match[1].downcase.gsub(' ', '-'))]
+                toc << "    + [%d.%d %s](#%s)\n" % [hlevel2, hlevel3, htext, hid]
             end
         end
+
         content.gsub('<!-- toc -->', toc)
     end
 end
